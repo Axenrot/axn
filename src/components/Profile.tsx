4fad3c28780b7feaa1c4703ab337aeff315eb3b4
Leon <yurileon@hotmail.com.br>
2023-06-14 23:30:10 -0300

    feat: profile menu done

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import ProfileInfo from "./ProfileMenu";
+import ProfilePicture from "./ProfilePicture";
+
+const Profile = () => {
+  return (
+    <div className="flex flex-col justify-center sm:justify-around sm:flex-row">
+      <ProfilePicture />
+      <ProfileInfo />
+    </div>
+  );
+};
+
+export default Profile;
