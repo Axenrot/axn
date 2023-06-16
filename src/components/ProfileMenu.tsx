18615af591a37e40c2b3efb3b17887788d658d21
Leon <yurileon@hotmail.com.br>
2023-06-16 02:36:31 -0300

    feat: touch screen check

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,10 +1,16 @@
+"use client";
 import MenuItem from "./MenuItem";
+import useTouchScreenCheck from "@/hooks/useTouchScreenCheck";
 
 const ProfileMenu = () => {
+  console.log();
   return (
     <div className="flex flex-col text-center">
-      <MenuItem message={"Hello."} button={"About me"} />
-      <MenuItem message={"I'm"} button={"Work"} />
+      <MenuItem
+        message={`Hello${useTouchScreenCheck() ? "" : "."}`}
+        button={"About me"}
+      />
+      <MenuItem message={"I'm"} button={"Works"} />
       <MenuItem message={"Leon"} button={"Contact"} />
     </div>
   );
