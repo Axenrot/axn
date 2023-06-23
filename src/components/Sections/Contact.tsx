039e0f21e620e0b7a1296d23e207abd30be8c52d
Leon <yurileon@hotmail.com.br>
2023-06-22 22:52:36 -0300

    feat: section background + about me

---
+++ src/components/Sections/Contact.tsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <div>
+      Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Laboriosam distinctio suscipit recusandae, voluptatem odio numquam ipsum
+      voluptatum debitis, officiis unde quaerat? Aut perspiciatis nostrum
+      repellendus placeat modi earum aspernatur voluptatem.
+    </div>
+  );
+};
+
+export default Contact;
