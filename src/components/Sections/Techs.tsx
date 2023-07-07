2117e27eb09dcea2108f2d12bcbb6dcbfb43c8a7
Leon <yurileon@hotmail.com.br>
2023-07-06 22:10:37 -0300

    feat: darkmode + gray img

--- src/components/Sections/Techs.tsx
+++ src/components/Sections/Techs.tsx
@@ -9,63 +9,64 @@ import git from "../../../public/images/tech/git.svg";
 import Image from "next/image";
 
 const Techs = () => {
-  // const size = 40;
   return (
     <div className="flex flex-row gap-[6px] md:gap-12 xl:gap-24 2xl:gap-32 items-center justify-between px-3 sm:px-12 py-6 font-semibold">
       <div id="react" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={react}
+          style={{ fill: "white" }}
           alt="React.js"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="javascript" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={javascript}
           alt="JavaScript"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="tailwindcss" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={tailwindcss}
           alt="TailwindCSS"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="typescript" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={typescript}
+          style={{ fill: "white" }}
           alt="TypeScript"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="next" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={next}
           alt="Next.js"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="node" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={node}
           alt="Node.js"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="git" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={git}
           alt="Git"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
       <div id="python" onMouseEnter={() => {}} className="w-fit">
         <Image
           src={python}
           alt="Python"
-          className="w-full transition-all duration-300 hover:scale-125"
+          className="w-full transition-all duration-300 filter invert dark:filter-none hover:scale-125"
         />
       </div>
     </div>
